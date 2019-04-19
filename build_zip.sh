7f7d5fecb15cbe9ab5f278b728ac91c0703f3b7f
ys197 <yuri@rus.net>
2019-04-19 19:11:05 +0300

    Added script for build .zip file

---
+++ build_zip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Script used in CI to build all configurations of mkspiffs
+#
+
+cp build/bootloader/bootloader.bin ./
+cp build/partitions.bin ./
+cp build/esp32_hd.bin ./
+cp build/spiffs.img ./
+zip fw_esp32_v02.zip bootloader.bin partitions.bin esp32_hd.bin spiffs.img
