0f540bd5573427b334953d7b28a61b69e29b1acb
ys1797 <yuri@rus.net>
2019-04-22 16:31:02 +0300

    New extended variable list and param_default();

--- main/config.c
+++ main/config.c
@@ -23,40 +23,80 @@ License (MIT license):
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <cJSON.h>
 
 #include "esp_log.h"
 #include "config.h"
 
 vaiable_list DEFL_PARAMS[] =
 {
-	{"maxPower",		VARIABLE_INT,	0,	15000,	"2000"},
-	{"ustPowerReg",		VARIABLE_INT,	0,	15000,	"900"},
-	{"tempEndRectRazgon",	VARIABLE_FLOAT, 0,	120,	"83.0"},
-	{"powerRect",		VARIABLE_INT,	0,	15000,	"1000"},
-	{"tEndRectOtbGlv",	VARIABLE_FLOAT, 0,	120,	"85.4"},
-	{"timeChimRectOtbGlv",	VARIABLE_INT,	0,	10000,	"20"},
-	{"procChimOtbGlv",	VARIABLE_INT,	0,	101,	"5"},
-	{"minProcChimOtbSR",	VARIABLE_INT,	0,	101,	"20"},
-	{"beginProcChimOtbSR",	VARIABLE_INT,	0,	100,	"40"},
-	{"timeChimRectOtbSR",	VARIABLE_INT,	0,	1500,	"10"},
-	{"tempDeltaRect",	VARIABLE_FLOAT, 0,	120,	"0.3"},
-	{"tempEndRectOtbSR",	VARIABLE_FLOAT, 0,	120,	"96.5"},
-	{"tempEndRect",		VARIABLE_FLOAT, 0,	120,	"99.5"},
-	{"p_MPX5010",		VARIABLE_INT,	0,	100,	"0"},
-	{"timeStabKolonna",	VARIABLE_INT,	0,	3500,	"900"},
-	{"timeRestabKolonna",	VARIABLE_INT,	0,	3500,	"1800"},
-	{"pShim", 		VARIABLE_INT,	0,	101,	"90"},
-	{"klpSilentNode", 	VARIABLE_CHECKBOX, 0,	1,	"1"},
-	{"urovenProvodimostSR", VARIABLE_INT,	0,	1000,	"0"},
-	{"cntCHIM", 		VARIABLE_INT,	-100,	100,	"-4"},
-	{"decrementCHIM", 	VARIABLE_INT,	0,	100,	"10"},
-	{"incrementCHIM", 	VARIABLE_INT,	0,	100,	"5"},
-	{"timeAutoIncCHIM", 	VARIABLE_INT,	0,	1000,	"600"},
-	{"alarmMPX5010", 	VARIABLE_INT,	0,	100,	"0"},
-	{"beepChangeState", 	VARIABLE_CHECKBOX, 0,	1,	"1"},
+	{"maxPower",		VARIABLE_INT,	0,	15000,	"2000",	NULL},
+	{"ustPowerReg",		VARIABLE_INT,	0,	15000,	"900",	NULL},
+	{"tempEndRectRazgon",	VARIABLE_FLOAT, 0,	120,	"83.0",	NULL},
+	{"powerRect",		VARIABLE_INT,	0,	15000,	"1000",	NULL},
+	{"tEndRectOtbGlv",	VARIABLE_FLOAT, 0,	120,	"85.4",	NULL},
+	{"timeChimRectOtbGlv",	VARIABLE_INT,	0,	10000,	"20",	 NULL},
+	{"procChimOtbGlv",	VARIABLE_INT,	0,	101,	"5",	NULL},
+	{"minProcChimOtbSR",	VARIABLE_INT,	0,	101,	"20",	NULL},
+	{"beginProcChimOtbSR",	VARIABLE_INT,	0,	100,	"40",	NULL},
+	{"timeChimRectOtbSR",	VARIABLE_INT,	0,	1500,	"10",	NULL},
+	{"tempDeltaRect",	VARIABLE_FLOAT, 0,	120,	"0.3",	NULL},
+	{"tempEndRectOtbSR",	VARIABLE_FLOAT, 0,	120,	"96.5", NULL},
+	{"tempEndRect",		VARIABLE_FLOAT, 0,	120,	"99.5", NULL},
+	{"p_MPX5010",		VARIABLE_INT,	0,	100,	"0",	NULL},
+	{"timeStabKolonna",	VARIABLE_INT,	0,	3500,	"900",	NULL},
+	{"timeRestabKolonna",	VARIABLE_INT,	0,	3500,	"1800", NULL},
+	{"pShim", 		VARIABLE_INT,	0,	101,	"90",	NULL},
+	{"klpSilentNode", 	VARIABLE_CHECKBOX, 0,	1,	"1",	NULL},
+	{"urovenProvodimostSR", VARIABLE_INT,	0,	1000,	"0",	NULL},
+	{"cntCHIM", 		VARIABLE_INT,	-100,	100,	"-4",	NULL},
+	{"decrementCHIM", 	VARIABLE_INT,	0,	100,	"10",	NULL},
+	{"incrementCHIM", 	VARIABLE_INT,	0,	100,	"5",	NULL},
+	{"timeAutoIncCHIM", 	VARIABLE_INT,	0,	1000,	"600",	NULL},
+	{"alarmMPX5010", 	VARIABLE_INT,	0,	100,	"0",	NULL},
+	{"beepChangeState", 	VARIABLE_CHECKBOX, 0,	1,	"1",	NULL},
 
-	{"powerDistil",		VARIABLE_INT,	0,	15000,	"1000"},
-	{"tempEndDistil",	VARIABLE_FLOAT,	0,	120,	"99.5"},
+	{"powerDistil",		VARIABLE_INT,	0,	15000,	"1000", NULL},
+	{"tempEndDistil",	VARIABLE_FLOAT,	0,	120,	"99.5", NULL},
 
 	{NULL}
 };
+
+/* —брос параметров в значение по умолчанию */
+int param_default(void)
+{
+	FILE *f = fopen(RECT_CONFIGURATION, "w");
+	vaiable_list *v;
+	int  i;
+	double d;
+
+	if (f) {
+		cJSON *ja = cJSON_CreateObject();
+		for (v = DEFL_PARAMS; v && v->name; v++) {
+			switch (v->type) {
+			case VARIABLE_CHECKBOX:
+				i = atoi(v->default_val);
+				cJSON_AddItemToObject(ja, v->name, cJSON_CreateBool(i));
+				break;
+			case VARIABLE_STRING:
+				cJSON_AddItemToObject(ja, v->name, cJSON_CreateString(v->default_val));
+				break;
+			case VARIABLE_INT:
+				i = atoi(v->default_val);
+				cJSON_AddItemToObject(ja, v->name, cJSON_CreateNumber(i));
+				break;
+			case VARIABLE_FLOAT:
+				d = atof(v->default_val);
+				cJSON_AddItemToObject(ja, v->name, cJSON_CreateNumber(d));
+				break;
+			}
+		}
+		char *r=cJSON_Print(ja);
+		fprintf(f, "%s", r);
+		if (r) free(r);
+		cJSON_Delete(ja);
+		fclose(f);
+	}
+	return 0;
+}
+
