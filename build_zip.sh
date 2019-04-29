c3776e4b7a99f08849b613ec27530db6855c37af
ys1797 <yuri@rus.net>
2019-04-29 16:14:41 +0300

    Update script pathnames

--- build_zip.sh
+++ build_zip.sh
@@ -3,8 +3,8 @@
 # Script used in CI to build all configurations of mkspiffs
 #
 
-cp build/bootloader/bootloader.bin ./
-cp build/partitions.bin ./
-cp build/esp32_hd.bin ./
-cp build/spiffs.img ./
-./zip fw_esp32_v06.zip bootloader.bin partitions.bin esp32_hd.bin spiffs.img
+cp build/bootloader/bootloader.bin ./bin/
+cp build/partitions.bin ./bin/
+cp build/esp32_hd.bin ./bin/
+cp build/spiffs.img ./bin/
+./zip -j ./bin/fw_esp32_v06.zip ./bin/bootloader.bin ./bin/partitions.bin ./bin/esp32_hd.bin ./bin/spiffs.img
