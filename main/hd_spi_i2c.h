4b8ef947c8b339c1d7fec52861775fd963e26991
ys1797 <yuri@rus.net>
2020-03-04 17:03:49 +0300

    added extern "C" {}  Added new spi write functions for 16, 32 bit data type.  Added lcd detector code.

--- main/hd_spi_i2c.h
+++ main/hd_spi_i2c.h
@@ -20,9 +20,27 @@ License (MIT license):
   THE SOFTWARE.
 */
 
+#ifndef HD_SPI_I2C_h
+#define HD_SPI_I2C_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "driver/spi_master.h"
+#include "driver/i2c.h"
+
+typedef enum {
+	LCD_TYPE_ILI = 1,
+	LCD_TYPE_ST,
+	LCD_TYPE_MAX,
+} type_lcd_t;
+
 
 #define I2C_MASTER_NUM I2C_NUM_1   /*!< I2C port number for master dev */
 extern char I2C_detect[128];	/* detectecd i2c devices list */
+extern type_lcd_t lcd_type;	/* detected lcd type on SPI bus */
 
 uint8_t I2C_Init(i2c_port_t i2c_num, uint8_t _sda, uint8_t _scl);
 esp_err_t I2CWrite(uint8_t i2c_address, uint8_t* data_wr, size_t size);
@@ -31,5 +49,14 @@ esp_err_t I2CRead2(uint8_t i2c_add);
 void task_i2cscanner(void);
 
 void spi_setup(void);
-void lcd_cmd(const uint8_t cmd);
-void lcd_data(const uint8_t *data, int len) ;
+void spi_cmd(const uint8_t cmd);
+void spi_data(const uint8_t *data, int len) ;
+void spi_write16(const uint16_t w);
+void spi_write32(const uint32_t l);
+uint8_t spi_read8(uint8_t commandByte);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // HD_SPI_I2C_h
