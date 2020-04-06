96879115920ae71256a53e646fa81882aca1dbab
ys197 <yuri@rus.net>
2020-04-06 19:09:00 +0300

    strcmp() -> strcasecmp()

--- main/libesphttpd/httpd.c
+++ main/libesphttpd/httpd.c
@@ -85,7 +85,7 @@ const char ICACHE_FLASH_ATTR *httpdGetMimetype(char *url) {
 	if (*ext=='.') ext++;
 	
 	//ToDo: strcmp is case sensitive; we may want to do case-intensive matching here...
-	while (mimeTypes[i].ext!=NULL && strcmp(ext, mimeTypes[i].ext)!=0) i++;
+	while (mimeTypes[i].ext!=NULL && strcasecmp(ext, mimeTypes[i].ext)!=0) i++;
 	return mimeTypes[i].mimetype;
 }
 
@@ -194,7 +194,7 @@ int ICACHE_FLASH_ATTR httpdGetHeader(HttpdConnData *conn, char *header, char *re
 	while (p<(conn->priv->head+conn->priv->headPos)) {
 		while(*p<=32 && *p!=0) p++; //skip crap at start
 		//See if this is the header
-		if (strncmp(p, header, strlen(header))==0 && p[strlen(header)]==':') {
+		if (strncasecmp(p, header, strlen(header))==0 && p[strlen(header)]==':') {
 			//Skip 'key:' bit of header line
 			p=p+strlen(header)+1;
 			//Skip past spaces after the colon
@@ -618,12 +618,12 @@ static void ICACHE_FLASH_ATTR httpdParseHeader(char *h, HttpdConnData *conn) {
 		} else {
 			conn->getArgs=NULL;
 		}
-	} else if (strncmp(h, "Connection:", 11)==0) {
+	} else if (strncasecmp(h, "Connection:", 11)==0) {
 		i=11;
 		//Skip trailing spaces
 		while (h[i]==' ') i++;
 		if (strncmp(&h[i], "close", 5)==0) conn->priv->flags&=~HFL_CHUNKED; //Don't use chunked conn
-	} else if (strncmp(h, "Content-Length:", 15)==0) {
+	} else if (strncasecmp(h, "Content-Length:", 15)==0) {
 		i=15;
 		//Skip trailing spaces
 		while (h[i]==' ') i++;
@@ -644,7 +644,7 @@ static void ICACHE_FLASH_ATTR httpdParseHeader(char *h, HttpdConnData *conn) {
 			return;
 		}
 		conn->post->buffLen=0;
-	} else if (strncmp(h, "Content-Type: ", 14)==0) {
+	} else if (strncasecmp(h, "Content-Type: ", 14)==0) {
 		if (strstr(h, "multipart/form-data")) {
 			// It's multipart form data so let's pull out the boundary for future use
 			char *b;
