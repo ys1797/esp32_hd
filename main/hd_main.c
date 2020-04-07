0c3a5dd1d3ed33257cb4895f1375d51bb6aa2409
ys197 <yuri@rus.net>
2020-04-07 19:59:13 +0300

    Check and format nfs storage if not initialised.

--- main/hd_main.c
+++ main/hd_main.c
@@ -1560,7 +1560,11 @@ void app_main(void)
 	gpio_config_t io_conf;
 	esp_err_t ret;
 
-	nvs_flash_init();
+	ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
         ESP_LOGI(TAG, "RAM left %d", esp_get_free_heap_size());
 
 
